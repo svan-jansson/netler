d74c54f6a316a60a7147d9d051e7c03496838fc9
Svan Jansson <svan.jansson@gmail.com>
2019-11-26 04:47:05 +0100

    feat: Now generating elixir module that calls the generated dotnet project when running mix netler.new

--- lib/mix/tasks/netler.new.ex
+++ lib/mix/tasks/netler.new.ex
@@ -10,31 +10,53 @@ defmodule Mix.Tasks.Netler.New do
 
     case project_name do
       "" ->
-        Mix.Shell.IO.info([:red, "Aborting: No project name given."])
+        log_error("Aborting: No project name given.")
         :error
 
       project_name ->
         project_path = Dotnet.project_path(project_name)
-        create_dotnet_project(project_path, project_name)
-        Dotnet.compile_netler("#{project_path}/netler")
-        Mix.Shell.IO.info([:green, "Created new .NET project in #{project_path}"])
 
-        Mix.Shell.IO.info([
-          :yellow,
-          "Remeber to add :#{project_name} to the dotnet_projects list in your application's mix.exs"
-        ])
+        app = Mix.Project.config() |> Keyword.get(:app)
+        application_name = app |> Atom.to_string()
+        lib_path = "lib/#{application_name}"
+
+        create_source_files_from_templates(application_name, lib_path, project_path, project_name)
+
+        Dotnet.compile_netler("#{project_path}/netler")
+
+        log_info(
+          "Done! Remeber to add :#{project_name} to the dotnet_projects list in your application's mix.exs"
+        )
 
         :ok
     end
   end
 
-  defp create_dotnet_project(project_path, project_name) do
+  defp create_source_files_from_templates(application_name, lib_path, project_path, project_name) do
     File.mkdir_p!(project_path)
     csproj_file = "#{project_path}/#{Macro.camelize(project_name)}.csproj"
     program_file = "#{project_path}/Program.cs"
 
     File.write!(csproj_file, csproj_template())
+    log_info("Created #{csproj_file}")
+
     File.write!(program_file, program_template(project_name))
+    log_info("Created #{program_file}")
+
+    File.mkdir_p!(lib_path)
+    ex_file = "#{lib_path}/#{project_name}.ex"
+    File.write!(program_file, elixir_module_template(application_name, project_name))
+    log_info("Created #{ex_file}")
+  end
+
+  defp elixir_module_template(application_name, project_name) do
+    """
+    defmodule #{Macro.camelize(application_name)}.#{Macro.camelize(project_name)} do
+      use Netler, dotnet_project: :#{project_name}
+
+      def add(a, b), do: invoke("Add", [a, b])
+    end
+    """
   end
 
   defp csproj_template() do
@@ -85,5 +107,13 @@ defmodule Mix.Tasks.Netler.New do
         }
     }
     """
+  end
+
+  defp log_info(message) do
+    Mix.Shell.IO.info([:blue, message])
+  end
+
+  defp log_error(message) do
+    Mix.Shell.IO.info([:red, message])
   end
 end
--- lib/netler.ex
+++ lib/netler.ex
@@ -1,4 +1,15 @@
 defmodule Netler do
-  def invoke(project_name, method_name, parameters) when is_list(parameters),
-    do: Netler.Client.invoke(project_name, method_name, parameters)
+  defmacro __using__(opts) do
+    quote do
+      @dotnet_project Keyword.get(unquote(opts), :dotnet_project)
+
+      def invoke(method_name, parameters),
+        do: Netler.Client.invoke(@dotnet_project, method_name, parameters)
+
+      def invoke!(method_name, parameters) do
+        {:ok, response} = invoke(method_name, parameters)
+        response
+      end
+    end
+  end
 end
