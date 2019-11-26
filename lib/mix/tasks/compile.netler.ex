d15925444662ec4cd7707708ad7d027bc2e4ffcf
Svan Jansson <svan.jansson@gmail.com>
2019-11-26 04:05:18 +0100

    fix: Solved problem with binaries in root priv folder not being compied to _build priv folder on compilation

--- lib/mix/tasks/compile.netler.ex
+++ lib/mix/tasks/compile.netler.ex
@@ -5,9 +5,10 @@ defmodule Mix.Tasks.Compile.Netler do
 
   def run(_args) do
     File.mkdir_p!("priv")
+    config = Mix.Project.config()
 
     dotnet_projects =
-      Mix.Project.config()
+      config
       |> Keyword.get(:dotnet_projects, [])
 
     dotnet_projects
@@ -18,6 +19,25 @@ defmodule Mix.Tasks.Compile.Netler do
       Dotnet.compile_project(project_name)
     end)
 
+    symlink_or_copy(
+      config,
+      Path.expand("priv"),
+      Path.join(Mix.Project.app_path(config), "priv")
+    )
+
     :ok
+  end
+
+  defp symlink_or_copy(config, source, target) do
+    if config[:build_embedded] do
+      if File.exists?(source) do
+        File.rm_rf!(target)
+        File.cp_r!(source, target)
+      end
+
+      :ok
+    else
+      Mix.Utils.symlink_or_copy(source, target)
+    end
   end
 end
